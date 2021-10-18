76d2b4d1fc2f807e947b3aae40bf11340fdf29e0
Doommez <jenyaz862@gmail.com>
2021-10-18 16:40:53 +0300

    add DELETE, CHENGE

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const btnGetValue=document.querySelector("[name='getValue']")
 
 let mainContent=document.getElementById("main__content");
 let contentTask=document.querySelectorAll(".main__content__task");
-console.log(contentTask);
+
 
 btnGetValue.addEventListener("click",getValue)
 
@@ -20,28 +20,33 @@ if(JSON.parse( localStorage.getItem("taskArr"))!==null){
 else taskArr=[]
 
 function getValue(){
+  if(JSON.parse( localStorage.getItem("taskArr"))!==null){
+    taskArr=JSON.parse( localStorage.getItem("taskArr"));
+   
+}
     let value={
         date:date.value,
         name:name.value,
         priority:priority.value,
         description:description.value};
-    taskArr.push(value)
+    taskArr.push(value);
     let taskArrJson=JSON.stringify(taskArr)
    localStorage.setItem("taskArr",taskArrJson)
-   render([value],taskArr.length-1)
+   render([value],taskArr.length-1);
+   date.value=name.value=description.value=''
 }
 
 //{date.value,name.value,priority.value,description.value}
 
 function render(item,i){
-    console.log(i);
+  
    item.forEach((item,index,arr) => {
     mainContent.innerHTML+=` <div class="main__content__task" id="${i||index}">
     <div class="main__content__taskName">
       <div class="main__content__date">${item.date}</div>
       <div class="main__content__name">${item.name}</div>
       <div class="main__content__priority">${item.priority}</div>
-      <div class="main__content__change">
+      <div class="main__content__change" onclick="change(${i||index})">
         <img src="change.png" alt="change" />
       </div>
     </div>
@@ -49,10 +54,62 @@ function render(item,i){
       <div class="main__content__description">
       ${item.description}
       </div>
-      <input type="button" value="Delete" />
+      <input type="button" value="Delete" onclick="del(${i||index})"/>
     </div>`
    });
 }
 
 
 
+function del(i){
+ let newTaskArr=JSON.parse(localStorage.getItem("taskArr"));
+ console.log(newTaskArr);
+ newTaskArr.splice(i,1);
+ console.log(newTaskArr);
+ localStorage.setItem("taskArr",JSON.stringify(newTaskArr)) 
+ mainContent.innerHTML="";
+ render(JSON.parse( localStorage.getItem("taskArr")))
+}
+
+let changeBolean=false;
+function change(i){
+  let changeContentTask=document.getElementById(i);
+
+  let changeContent=changeContentTask.querySelectorAll(".main__content__description")
+
+changeContent[0].style.cssText='border: solid red 1px; background-color: blue;';
+changeContent[0].setAttribute("contenteditable","true");
+changeContent[0].focus()
+console.log(changeContent[0].textContent);
+let chek=document.createElement("div")
+chek.className="confirmText"
+chek.setAttribute("contenteditable","false");
+chek.setAttribute("onclick","confirmText("+i+")");
+changeContent[0].appendChild(chek)
+changeContent[0].addEventListener("keydown",function(ev){
+  if(ev.key==="Enter"){
+    confirmText(i)
+  ev.preventDefault()
+  }
+   
+  
+  })
+}
+
+function confirmText(i){
+  
+    let changeContentTask=document.getElementById(i);
+
+    let changeContent=changeContentTask.querySelectorAll(".main__content__description")
+    
+   // localStorage.setItem("taskArr",JSON.stringify())
+ let addNewValue=JSON.parse( localStorage.getItem("taskArr"));
+ addNewValue[i].description=changeContent[0].textContent;
+ console.log(addNewValue);
+ localStorage.setItem("taskArr",JSON.stringify(addNewValue));
+ changeContent[0].style.cssText="";
+ changeContent[0].setAttribute("contenteditable","false");
+ let chek=changeContent[0].querySelector('.confirmText');
+console.log(chek);
+changeContent[0].removeChild(chek)
+}
